58681a62e94b04c4a460a111d99c1f178487a098
Marc-André Descôteaux <marcdescotes@gmail.com>
2023-09-01 15:50:26 -0400

    Almost finished TicTacToe

--- Algorithms.Test/Q3_TicTacToe_Test.cs
+++ Algorithms.Test/Q3_TicTacToe_Test.cs
@@ -1,14 +1,47 @@
-namespace Algorithms.Test;
+// namespace Algorithms.Test;
 
 
-[TestClass]
-public class Q3_TicTacToe_Test
-{
+// [TestClass]
+// public class Q3_TicTacToe_Test
+// {
 
-    [TestMethod]
-    public void TicTacToeIsValid()
-    {
- 
-    }
+//     [TestMethod]
+//     public void TestXWins()
+//     {
+//         // Arrange
+//         TicTacToe game = new TicTacToe();
 
-}
+//         // Act
+//         game.XPlay(0, 0);
+//         game.OPlay(1, 0);
+//         game.XPlay(0, 1);
+//         game.OPlay(1, 1);
+//         game.XPlay(0, 2);
+
+//         Player winner = game.TheWinnerIs();
+
+//         // Assert
+//         Assert.AreEqual(Player.X, winner);
+//     }
+
+//     [TestMethod]
+//     public void TestOWins()
+//     {
+//         // Arrange
+//         TicTacToe game = new TicTacToe();
+
+//         // Act
+//         game.XPlay(0, 0);
+//         game.OPlay(1, 0);
+//         game.XPlay(0, 1);
+//         game.OPlay(1, 1);
+//         game.XPlay(2, 0);+
+//         game.OPlay(1, 2);
+
+//         Player winner = game.TheWinnerIs();
+
+//         // Assert
+//         Assert.AreEqual(Player.O, winner);
+//     }
+// }
+
--- Algorithms/Q3_TicTacToe.cs
+++ Algorithms/Q3_TicTacToe.cs
@@ -4,19 +4,112 @@ using System;
 public class Q3_TicTacToe
 {
 
-public static void Main3(string[] args)
+static void Main_TicTacToe(string[] args)
+        {
+            TicTacToe game = new TicTacToe();
+
+            // Exemple de jeu
+            game.XPlay(0, 0);
+            game.OPlay(1, 0);
+            game.XPlay(0, 1);
+            game.OPlay(2, 0);   
+            game.XPlay(0, 2);
+
+            Player winner = game.TheWinnerIs();
+            Console.WriteLine("Le joueur gagnant est : " + winner);
+        }
+
+public enum Player
     {
-        Question3(34);
-
-
+        X,
+        O
     }
 
-
-
-public static void Question3(long num)
+    public enum SquareState
     {
-
+        Empty,
+        X,
+        O
     }
 
+    public class TicTacToe
+    {
+        private SquareState[,] board;
+        private Player currentPlayer;
 
+        public TicTacToe()
+        {
+            board = new SquareState[3, 3];
+            currentPlayer = Player.X;
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                for (int col = 0; col < 3; col++)
+                {
+                    board[row, col] = SquareState.Empty;
+                }
+            }
+        }
+
+        public void XPlay(int row, int col)
+        {
+            if (board[row, col] == SquareState.Empty && currentPlayer == Player.X)
+            {
+                board[row, col] = SquareState.X;
+                currentPlayer = Player.O;
+            }
+        }
+
+        public void OPlay(int row, int col)
+        {
+            if (board[row, col] == SquareState.Empty && currentPlayer == Player.O)
+            {
+                board[row, col] = SquareState.O;
+                currentPlayer = Player.X;
+            }
+        }
+
+        public Player NextPlayer()
+        {
+            return currentPlayer;
+        }
+
+        public Player TheWinnerIs()
+        {
+            // Check rows
+            for (int row = 0; row < 3; row++)
+            {
+                if (board[row, 0] != SquareState.Empty && board[row, 0] == board[row, 1] && board[row, 1] == board[row, 2])
+                {
+                    return board[row, 0] == SquareState.X ? Player.X : Player.O;
+                }
+            }
+
+            // Check columns
+            for (int col = 0; col < 3; col++)
+            {
+                if (board[0, col] != SquareState.Empty && board[0, col] == board[1, col] && board[1, col] == board[2, col])
+                {
+                    return board[0, col] == SquareState.X ? Player.X : Player.O;
+                }
+            }
+
+            // Check diagonals
+            if (board[0, 0] != SquareState.Empty && board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                return board[0, 0] == SquareState.X ? Player.X : Player.O;
+            }
+
+            if (board[0, 2] != SquareState.Empty && board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                return board[0, 2] == SquareState.X ? Player.X : Player.O;
+            }
+
+            return Player.X; // Default winner when no one has won yet
+        }
+    }
 }
