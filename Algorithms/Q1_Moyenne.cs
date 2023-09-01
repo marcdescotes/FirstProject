dd7c00fbcc12392eb7ec9d78e7283664be623e6b
Marc-André Descôteaux <marcdescotes@gmail.com>
2023-09-01 12:59:58 -0400

    Finalized Q1

--- Algorithms/Q1_Moyenne.cs
+++ Algorithms/Q1_Moyenne.cs
@@ -3,19 +3,18 @@ using System;
 
 public class Q1_Moyenne
 {
-    public static void Main(string[] args)
+    public static void Main_Moyenne(string[] args)
     {
         List<double> numbers = new List<double> { 3.1, 42.42, 151.60, 9.59, 11.31 };
         double average = CalculateAverage(numbers);
         Console.WriteLine($"La moyenne est : {average}");
     }
 
-
     public static double CalculateAverage(List<double> numbers)
     {
         if (numbers.Count == 0)
         {
-            return 0; // Moyenne indéfinie pour une liste vide, retourne 0 pour éviter les erreurs.
+            return 0;
         }
 
         double sum = 0;
